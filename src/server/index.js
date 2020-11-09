a2ded847d76296f29be4f98a1918cbcd3a319f15
In Seo <inseo.kr@gmail.com>
2020-11-08 17:05:17 -0800

    .

--- src/server/index.js
+++ src/server/index.js
@@ -8,6 +8,8 @@ app.namespace('/api', function () {
 	app.use(express.static('dist'));
 	app.get('/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
 
-	app.listen(process.env.PORT || 8080, () => console.log(`Listening on port ${process.env.PORT || 8080}!`));
+	//app.listen(process.env.PORT || 8080, () => console.log(`Listening on port ${process.env.PORT || 8080}!`));
 	//app.listen(3000, () => console.log(`Listening on port 3000!`));
+    app.listen(8000, () => console.log(`Listening on port 8000!`));
+
 });
